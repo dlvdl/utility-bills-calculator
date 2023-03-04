9c8c681288426c46db51fe951db6049583470185
DenysLygach <denisligach98@gmail.com>
2023-03-04 13:47:31 +0200

    simplify settings component

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -5,89 +5,49 @@ function Settings() {
   const [showSettings, setSettings] = useState(false)
   const { getSettings } = useContext(GlobalContext)
   const settings = getSettings()
+
   const clickHandler = (e) => {
     setSettings(!showSettings)
   }
+
   console.log(getSettings())
 
   return (
     <div className="container">
       <div className="flex f-direction-column">
         <h3>Settings</h3>
-        {showSettings ? (
-          <div>
-            <form action="">
-              <table>
-                <tbody>
-                  <tr>
-                    <th>Category</th>
-                    <th>Start amount</th>
-                    <th>Units</th>
-                    <th>Tariff</th>
-                  </tr>
-                  <tr>
-                    <td>Water</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                    <td>m3</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Natural gas</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                    <td>m3</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Electricity</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                    <td>kwt</td>
-                    <td>
-                      <input type="text" />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </form>
-            <button>Save</button>
-            <button onClick={clickHandler}>Back</button>
-          </div>
-        ) : settings ? (
-          <div>
+        <div>
+          <form action="">
             <table>
               <tbody>
                 <tr>
                   <th>Category</th>
                   <th>Start amount</th>
-                  <th>Units</th>
                   <th>Tariff</th>
                 </tr>
-                {settings.map((value) => {
-                  return (
-                    <tr key={value.id}>
-                      <td>{value.type}</td>
-                      <td>{value.amount}</td>
-                      <td>m3</td>
-                      <td>{value.tariff}</td>
-                    </tr>
-                  )
-                })}
+                {settings ? (
+                  settings.map((value) => {
+                    return (
+                      <tr key={value.id}>
+                        <td>{value.type}</td>
+                        <td>
+                          <input type="text" defaultValue={value.amount} />
+                        </td>
+
+                        <td>
+                          <input type="text" defaultValue={value.tariff} />
+                        </td>
+                      </tr>
+                    )
+                  })
+                ) : (
+                  <h2>Settings not found</h2>
+                )}
               </tbody>
             </table>
-            <button onClick={clickHandler}>ADD</button>
-          </div>
-        ) : (
-          <p>No settings</p>
-        )}
+            <button onClick={clickHandler}>Change</button>
+          </form>
+        </div>
       </div>
     </div>
   )
